8dd10833720bdbe79496aba8e50b24c2c27fbcdf
Muhammad Zubair (DevMZ) <30407461+MuhammadZubair210@users.noreply.github.com>
2022-10-03 23:53:27 -0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "127.0.0.1", // hosting address
       port: 7545,
       network_id: "*" // Match any network id
     }
